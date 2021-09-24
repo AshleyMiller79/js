0646022693ea5d69420c18ca003f6a71e84c14ba
ipek <ipekbilir79@gmail.com>
2021-09-24 23:35:21 +0300

    e

---
+++ ders4_dom/ornek1_selector/app.js
@@ -0,0 +1,7 @@
+//================================================================ =====
+//                   GETELEMENTID
+
+const paragraf = document.getElementById("para").style;
+paragraf.backgroundColor = "black";
+paragraf.color = "white";
+paragraf.fontSize = "20px";
