9fd5c5ee02bdc7f0d1702202fa901130547074f3
ipek <ipekbilir79@gmail.com>
2021-09-25 18:07:22 +0300

    ww

--- ders4_dom/ornek1_selector/app.js
+++ ders4_dom/ornek1_selector/app.js
@@ -1,7 +1,62 @@
 //================================================================ =====
-//                   GETELEMENTID
+//                   GETELEMENTBYID
 
 const paragraf = document.getElementById("para").style;
 paragraf.backgroundColor = "black";
 paragraf.color = "white";
 paragraf.fontSize = "20px";
+
+
+const buton=document.getElementById("btn");
+buton.style.width="100px";
+buton.style.color="red";
+buton.style.fontSize="18px";
+buton.textContent="ARA";
+//! *********textContent=tag lerin içeriğindeki yazıyı değiştirmek istersek
+
+
+//***** GETELEMENTBYTAGNAME */ bu kullanım tercih edilmiyor, class adı =tag adı
+
+const resim=document.getElementsByTagName("img");
+
+resim[0].style.width="300px";
+resim[0].style.height="300px";
+resim[1].style.border="3px solid red";
+
+//*****GETELEMENTBYCLASSNAME */
+
+const baslık=document.getElementsByClassName("h1");
+baslık[0].style.textAlign="center";
+
+// *********QUERYSELECTOR*********
+//! en çok kullanılan çağırma yolu
+
+document.querySelector(".arama").textContent="DOM SELECTOR KAVRAMI👋";
+
+document.querySelector("#govde").style.backgroundImage="linear-gradient(green, blue, pink)";
+
+
+
+
+//*****EVENT****** */
+
+//(onmouseover, onmouseout =fare üstüne gelince ayrılınca)
+const h= document.querySelector(".h1");
+
+
+h.onmouseover =function(){
+h.style.color="red";
+h.style.backgroundColor="white";
+
+}
+
+
+
+
+
+
+
+
+
+
+
