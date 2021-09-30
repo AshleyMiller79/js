cc452614f89f5df3fc88fa26c0807981f435538e
ipek <ipekbilir79@gmail.com>
2021-09-30 22:31:55 +0300

    t

--- ders5_diziler/ornek3_iterasyon/iterasyon.js
+++ ders5_diziler/ornek3_iterasyon/iterasyon.js
@@ -15,11 +15,10 @@
 //****FOR IN DÖNGÜSÜ*********** */
 const koordinatlar = [120, 100, -100, 220, 330, -50];
 
-
 let pozitif = 0;
 let negatif = 0;
 
-for (let i in  koordinatlar) {
+for (let i in koordinatlar) {
   koordinatlar[i] < 0
     ? (negatif = negatif + koordinatlar[i])
     : (pozitif = pozitif + koordinatlar[i]);
@@ -32,7 +31,15 @@ console.log(`negatifler toplamı: ${negatif} pozifler toplamı: ${pozitif}`);
 //  ana programa döndürecek fonksiyonu yaziniz. Eğer o
 // türden bir hayvan yok ise bulunamadığını yazdırsın.
 
-const hayvanlar=[ "aslan", "fil", "kartal" , "kanarya", "aslan", "aslan", "timsah"];
+const hayvanlar = [
+  "aslan",
+  "fil",
+  "kartal",
+  "kanarya",
+  "aslan",
+  "aslan",
+  "timsah",
+];
 
 // const ara=prompt("sayısını merak ettigin hayvanın adını yaz");
 // const bulDondur=function (ara) {
@@ -48,34 +55,29 @@ const hayvanlar=[ "aslan", "fil", "kartal" , "kanarya", "aslan", "aslan", "timsa
 // }
 // bulDondur(ara);
 
-
-
 //******FOR OF DONGUSU****** */
-
 
 // for of dongusu, for in dongusunun bir cok veri yapisini kapsayacak sekilde guncellenmis halidir. Bu dongude dizi icersindeki veriye erisirken indisleme kullanmaya gerek yoktur.
 
-let arabalar = [ "BMW", "Volvo", "Mini"];
+let arabalar = ["BMW", "Volvo", "Mini"];
 
-for(let i of arabalar){
-
- console.log(i);
+for (let i of arabalar) {
+  console.log(i);
 }
 
 //**** FOREACH METODU********** */
 //! foreach metodu orjinal diziyi bozmaz
 
-let ogrenciler =["Onur", "Hakan", "Furkan"];
+let ogrenciler = ["Onur", "Hakan", "Furkan"];
 
-console.log( ogrenciler.forEach((ogrenci) => console.log(ogrenci)));
- //undefined
+console.log(ogrenciler.forEach((ogrenci) => console.log(ogrenci)));
+//undefined
 
-ogrenciler.forEach((deger, index)=>console.log(index + ". kişi " + deger +"'dır"));
+ogrenciler.forEach((deger, index) =>
+  console.log(index + ". kişi " + deger + "'dır")
+);
 
-
-console.log(ogrenciler);  //["Onur", "Hakan", "Furkan"];
-
-
+console.log(ogrenciler); //["Onur", "Hakan", "Furkan"];
 
 //*******MAP METODU***** */
 
@@ -83,28 +85,74 @@ console.log(ogrenciler);  //["Onur", "Hakan", "Furkan"];
 
 //ornek: bütün elemanların 3 katını alınız
 
-const rakamlar= [3, 5, 3, 2, 6, 7, 9];
+const rakamlar = [3, 5, 3, 2, 6, 7, 9];
 
 // console.log( rakamlar.map((x)=> x*3));
 
-console.log( rakamlar.map((x, index)=> `${index+1}. elemanın 3 katı ${x*3}`));
+console.log(
+  rakamlar.map((x, index) => `${index + 1}. elemanın 3 katı ${x * 3}`)
+);
+
+//ornek
+
+//isimler dizisinin içerisindeki her ismi büyük harf olarak yeni bir diziye sakla
+
+const isimler = [
+  "Ahmet",
+  "mehmet",
+  "ismet",
+  "SAFFET",
+  "Can",
+  "Canan",
+  "Cavidan",
+];
+
+// map metodu bir dizi döndürür
+
+const buyuk= isimler.map((isim) => isim.toUpperCase());
+console.log(buyuk);
+ 
+document.querySelector(".baslık").textContent =buyuk.join(" -> ");
+
+// *****FİLTER METODU*********
+
+ //ornek 
+
+ //fiyatlar dizisindeki 250 den büyük fiyatları yazdır
+
+ const fiyatlar= [120, 260, 300 ,322.5, 400 , 768];
+
+ console.log( fiyatlar.filter((t)=> t > 250));
+
+//-------------- ÖRNEK -------------------
+// maası 4000 $'dan düsük olanlara %50 zam yapmak istiyoruz
+// ve bunu ayri dizi olarak saklamak istiyoruz.
+
+const maaslar = [3000, 5000, 4000, 6000, 6500];
+const yeni=maaslar.filter((a) => a <= 4000).map((a) => a * 1.5);
+console.log(yeni);
+
+// console.log( maaslar.filter((a)=>a<=4000).map((a)=>a*1.5));
 
 
+//-------------- ÖRNEK -------------------
 
+const adlar = [
+  "Samet",
+  "Hakkı",
+  "Duygu",
+  "Emrullah",
+  "Bilal",
+  "Ali",
+  "Ahmet",
+  "Hasan",
+  "Defne",
+  "Serdar",
+];
 
+//ilk harfi A olanları konsol da yazdır
 
-
-
-
-
-
-
-
-
-
-
-
-
+adlar.filter((x)=>x.startsWith("A")).forEach((x)=>console.log(x));
 
 
 
