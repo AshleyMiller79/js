ec27bc979b2a6ab14d0dab8d27fff5d8597424ef
ipek <ipekbilir79@gmail.com>
2021-09-30 23:44:29 +0300

    r

---
+++ ders5_diziler/ornek4_dizilerDom/app.js
@@ -0,0 +1,31 @@
+const maaslar= [ 4000, 5000, 3500, 4200, 2850, 5000, 7500];
+
+const liste=document.querySelector(".liste-ul");
+
+for(let i of maaslar) {
+liste.innerHTML=liste.innerHTML + `<li> ${i} </li>` 
+
+}
+// for (let i in maaslar) {
+//   liste.innerHTML = liste.innerHTML + `<li> ${maaslar[i]} </li>`;
+// }
+
+//ekle butonuna tıkladığımızda diziye ve listeye yeni veri ekle 
+const giris= document.querySelector(".liste");
+
+document.querySelector(".ekle").onclick=function(){
+liste.innerHTML=liste.innerHTML + `<li> ${giris.value} </li>`
+ giris.value="";
+
+maaslar.push(giris.value);
+
+}
+
+//Sil butonuna tiklandiginda listedeki ve dizideki son elamani sil.
+document.querySelector(".sil").onclick = function () {
+  if (maaslar.length == 0) {
+    alert("Silinecek oge kalmadi.");
+  } else {
+    maaslar.pop();
+     liste.removeChild(liste.lastElementChild);
+  }};
