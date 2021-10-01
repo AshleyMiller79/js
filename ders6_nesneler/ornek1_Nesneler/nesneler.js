b7ae35da4616ad53b538ce2de507454d52596c0e
ipek <ipekbilir79@gmail.com>
2021-10-01 22:38:51 +0300

    e

--- ders6_nesneler/ornek1_Nesneler/nesneler.js
+++ ders6_nesneler/ornek1_Nesneler/nesneler.js
@@ -29,6 +29,10 @@ console.log(insan["diller"]);
 
 console.log(`Adım: ${insan.ad} ve yasım: ${insan.yas}`);
 
+//************* */
+// const bilgi= prompt("insan nesnesinden hangisini görmek istersin - ad, soyad, yas..- ");
+// console.log(insan[bilgi]);
+
 //objeye key.value lar ekleme
 
 insan.konum="Türkiye";
@@ -61,6 +65,80 @@ console.log(kisi);
 console.log(kisi.yasHesapla());
 
 console.log(kisi.ozet());
+
+
+// Nesne (object) iterasyon örnekleri
+
+const people= [
+
+{
+ad:"hakan",
+soyad:"inal",
+meslek:"developer",
+yas:41
+
+},
+
+{
+ad:"said",
+soyad:"cin",
+meslek:"tester",
+yas:37
+
+},
+{
+ad:"kubilay",
+soyad:"tuncel",
+meslek:"team lead",
+yas:33
+
+},
+
+{
+ad:"osman",
+soyad:"Türker",
+meslek:"grafiker",
+yas:29
+
+},
+{
+ad:"hüseyin",
+soyad:"harran",
+meslek:"doktor",
+yas:32
+}
+];
+
+console.log(people);
+
+//ornek:1 people dizisindeki kişilerin mesleklerini konsol da yazdıralım
+
+people.forEach((x)=>console.log(x.meslek));
+
+
+//2) people dizisineki tüm kişilerin yaslarını 1 arttırarak yazdırınız
+
+people.map((x)=>x.yas+1).forEach((x)=>console.log(x));
+
+//ornek3) yası 35 e esit veya kücük olanların adlarını yazdır
+
+people.filter((x)=>x.yas <=35).forEach((x)=>console.log(x.ad));
+
+//ornek4) people dizisindeki kişilerin isimlerini büyük harf olarak alan ve yaslarını 5 arttıran ve soyadının ilk 2 harfini yazdıran fonksiyonu yazdırınız
+
+
+const degisiklik = people.map((x) => {
+  return {
+    
+    ad: x.ad.toUpperCase(),
+    yas: x.yas + 5,
+    soyad: x.soyad.slice(0, 2),
+  };
+});
+
+
+
+
 
 
 
