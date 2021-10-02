34afdb9397a7bb2b6405a84626b12fc3c9109c97
ipek <ipekbilir79@gmail.com>
2021-10-02 23:27:06 +0300

    t

---
+++ hesapMakinesi/app.js
@@ -0,0 +1,21 @@
+
+function hesapEt(sonuc) {
+ if(form.displayResult.value==0){
+  form.displayResult.value=sonuc;
+ }else
+{
+form.displayResult.value=form.displayResult.value+sonuc;}
+}
+
+
+
+
+
+
+
+
+document.querySelector(".bos").onclick=function(){
+document.querySelector(".ekranBorder").value="";
+
+}
+
