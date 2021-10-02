032017ad34e2a177efb6b7cd564d4421aec9bf71
ipek <ipekbilir79@gmail.com>
2021-10-02 23:51:40 +0300

    t

---
+++ tvShow/script.js
@@ -0,0 +1,30 @@
+var showListe=[];
+
+//! json dosyasından veri getirme aşamaları
+
+fetch("./tv-shows.json").then((cevap)=>cevap.json()).then((veri)=>{
+
+
+showListe=veri;
+
+showIzle(showListe); //method call
+
+});
+
+//metod oluştur
+function showIzle(showListe){
+
+  //!https://getbootstrap.com/docs/4.0/components/card/
+
+  var liste = document.querySelector(".liste");
+  showListe.forEach((a) => {
+    liste.innerHTML = liste.innerHTML + `<div class=  "card col-md-3" >
+  <img class="card-img-top" src=${a.image.medium} alt="">
+  <div class="card-body">
+    <h5 >${a.name}</h5>
+    
+    <a href=${a.url} target="_blank" class="btn btn-success">Detaylar</a>
+  </div>
+</div>`;
+  });
+ }
